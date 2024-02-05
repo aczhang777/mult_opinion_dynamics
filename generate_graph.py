2c2570491f166c234f39a24e206de6f9796162d3
Annie Zhang <aczhang777@gmail.com>
2024-02-04 21:48:34 -0500

    updates
    for generate_graph: changed graph generation process to account for toggling edge density within the English and Chinese speakers and between.
    for simulate: got started on it yay!

--- generate_graph.py
+++ generate_graph.py
@@ -1,80 +1,145 @@
 import networkx as nx
 import random as rand 
 
-# probably turn this into a function so you can call it in other files
+# when you do this, remember to implement a check that b <= n2
+# also remember to allow for changes in whether we draw from normal or uniform distribution
+# also at one point consider allowing for drawing opinions from a 2D space
 
-# create a list of nodes in dictionary format: keys are nodes (numbered), values are node attributes (political ideology, bilingual status, willingness to share and consume content in other languages, willingness to translate information before sharing)
-G = nx.Graph()
-n = 100 # number of nodes in G
-p = 0.5 # probability of an edge between any pair (no self loops allowed)
+# WHAT IF WE ALLOW FOR ABCS AND CREATE A THIRD GROUP OF INDIVIDUALS
+# could be a good follow up / extension!
+# WHAT IF WE ALLOW FOR MULTIPLE LAYERED GRAPHS TO ACCOUNT FOR DIVERSE SOCIAL MEDIA USAGE
+# again could be a great follow up / extension!
 
-### nodes!
-# keys will be 0 through n-1, values dictionaries with 5 key value pairs, for now we'll draw nums randomly from uniform or gauss dist!
-node_attributes_dictionary = {}
+def generate_graph_from_parameters(n1: int, n2: int, b: int, p1: float, p2: float, p3: float):
 
-for i in range(0, n):
-    # draw 5 random numbers 
-    attributes = {}
-    # first number is political ideology on [-1, 1]
-    while len(attributes) < 1:
-        num = rand.gauss(mu = 0.0, sigma = 0.33)
-        if -1 <= num <= 1:
-            attributes["Political Ideology"] = num
+    # First we create a list of nodes in dictionary format: keys are nodes (numbered), values are node attributes (political ideology, bilingual status, willingness to share and consume content in other languages, willingness to translate information before sharing)
+    G = nx.Graph()
+    n1 = n1 # number of English speakers, int
+    n2 = n2 # number of Chinese speakers, int
+    n = n1 + n2 # number of nodes in G, int
+    b = b # number of bilingual Chinese speakers, b < n2, int
+    p1 = p1 # probability of an edge between any two English speakers, float
+    p2 = p2 # probability of an edge between any two Chinese speakers, float
+    p3 = p3 # probability of an edge between any English speaker and any Chinese speaker, float
+
+    # Here we create the node attributes, first for all the English speakers and then the Chinese speaakers
+
+    # keys will be 0 through n-1, values will be dictionaries with 5 key value pairs, for now we'll draw nums randomly from uniform or gauss dist!
+    node_attributes_dictionary = {}
+
+    for i in range(0, n1):
+        attributes = {} # fill in this dictionary with each English speaker's attributes
+
+        # first is political ideology
+        while len(attributes) < 1:
+            num = rand.gauss(mu = 0.0, sigma = 0.33)
+            if -1 <= num <= 1:
+                attributes["Political Ideology"] = num
     
-    # second is mother tongue, either 0 (English equivalent) or 1 (Chinese equivalent)
-    num = rand.random()
-    if num < 0.5:
-        attributes["Mother Tongue"] = "English" 
-    else:
+        # second is their mother tongue
+        attributes["Mother Tongue"] = "English"
+
+        # third is the fact that they are not bilingual
+        attributes["Bilingual Status"] = "Not Bilingual"
+
+        # fourth is the fact that they are not willing to receive foreign info
+        attributes["Receive Foreign Info"] = 0
+
+        # fifth is the fact that they are not going to translate and share any information
+        attributes["Translate and Share"] = 0
+
+        # now we add this dictionary to our larger dictionary
+        node_attributes_dictionary[i] = attributes
+
+    # from the list of Chinese speakers we are going to uniformly at random pick b of them to be bilingual
+    chinese_speakers = list(range(n1, n))
+    bilinguals = rand.sample(chinese_speakers, b)
+
+    # now we create and add attributes for each Chinese speaker
+    for i in range(n1, n):
+        attributes = {} # fill in this dictionary with each Chinese speaker's attributes
+        # first number is political ideology on [-1, 1]
+        while len(attributes) < 1:
+            num = rand.gauss(mu = 0.0, sigma = 0.33)
+            if -1 <= num <= 1:
+                attributes["Political Ideology"] = num
+        
+        # mother tongue is Chinese
         attributes["Mother Tongue"] = "Chinese"
 
-    # third: if mother tongue is English, then all the rest are 0. If mother tongue is Chinese, then third is whether or not they are bilingual
-    if attributes.get("Mother Tongue") == "English": 
-        attributes["Bilingual Status"] = "Not Bilingual"
-    else: 
-        num = rand.random()
-        if num < 0.5:
-            attributes["Bilingual Status"] = "Not Bilingual" 
-        else:
+        # must specify bilingual status for each Chinese speaking node
+        if i in bilinguals:
             attributes["Bilingual Status"] = "Bilingual"
+        else:
+            attributes["Bilingual Status"] = "Not Bilingual"
 
-    # fourth: if bilingual is 0, all the rest are 0. Otherwise, fourth is how willing they are to consume/receive content when it's not in their mother tongue
-    if attributes.get("Bilingual Status") == "Not Bilingual": # if they are not bilingual
-        attributes["Receive Foreign Info"] = 0
-    else: # if they are bilingual
-        while len(attributes) < 4:
-            num = rand.gauss(mu = 0.5, sigma = 0.1667)
-            if 0 <= num <= 1:
-                attributes["Receive Foreign Info"] = num
+        # fourth: if not bilingual, then this attribute is 0. Otherwise, fourth is how willing they are to consume/receive content when it's not in their mother tongue
+        if attributes.get("Bilingual Status") == "Not Bilingual": # if they are not bilingual
+            attributes["Receive Foreign Info"] = 0
+        else: # if they are bilingual
+            while len(attributes) < 4:
+                num = rand.gauss(mu = 0.5, sigma = 0.1667)
+                if 0 <= num <= 1:
+                    attributes["Receive Foreign Info"] = num
 
-    # fifth: if bilingual is 0, then this last one is also 0. Otherwise, fifth is how willing they are to translate and share content that's not in their mother tongue
-    if attributes.get("Bilingual Status") == "Not Bilingual":
-        attributes["Translate and Share"] = 0
-    else:
-        while len(attributes) < 5:
-            num = rand.gauss(mu = 0.5, sigma = 0.1667)
-            if 0 <= num <= 1:
-                attributes["Translate and Share"] = num
-    
-    node_attributes_dictionary[i] = attributes
+        # fifth: if not bilingual, then this attribute is 0. Otherwise, fifth is how willing they are to translate and share content that's not in their mother tongue
+        if attributes.get("Bilingual Status") == "Not Bilingual":
+            attributes["Translate and Share"] = 0
+        else:
+            while len(attributes) < 5:
+                num = rand.gauss(mu = 0.5, sigma = 0.1667)
+                if 0 <= num <= 1:
+                    attributes["Translate and Share"] = num
+        
+        node_attributes_dictionary[i] = attributes
 
-# add these nodes and node attributes to G
-G.add_nodes_from(list(range(n)))
-nx.set_node_attributes(G, node_attributes_dictionary, "Node Attributes")
+    # add these nodes and node attributes to G
+    G.add_nodes_from(list(range(n)))
+    nx.set_node_attributes(G, node_attributes_dictionary, "Node Attributes")
 
-print(G.nodes[0])
-print(G.nodes[15])
-print(G.nodes[46])
+    #print(bilinguals)
+    #for i in bilinguals:
+        #print(G.nodes[i]["Node Attributes"]["Bilingual Status"])
+    #print(list(G.nodes(data = True)))
+    #for node in list(G.nodes(data = True)):
+        #print(node)
 
-### edges!
-edges = [] #fill in this list
+    # Here is where we create our list of edges
+    edges = [] #fill in this list
 
-for i in range(0, n-1): # yes this is correct, it's not n but n-1
-    for j in range(i+1, n):
-        num = rand.random()
-        if num < p: # add an edge!
-            edges.append((i, j))
+    # first we will add in the edges between the English speaking nodes
+    for i in range(0, n1-1): # starts at 0 and ends at 4
+        for j in range(i+1, n1): # starts at 1 and ends at 5
+            num = rand.random()
+            if num < p1:
+                edges.append((i, j))
 
-G.add_edges_from(edges)
+    # now we'll add in the edges between the Chinese speaking nodes
+    for i in range(n2, n-1): # starts at 6 and ends at 10
+        for j in range(i+1, n): # starts at 7 and ends at 11
+            num = rand.random()
+            if num < p2:
+                edges.append((i, j))
+
+    # lastly we add in the edges between English and Chinese speaking nodes
+    for i in range(0, n1): # starts at 0 and ends at 5
+        for j in range(n2, n): # starts at 6 and ends at 11
+            num = rand.random()
+            if num < p3:
+                edges.append((i, j))
+
+    G.add_edges_from(edges)
+
+    #print(edges)
+
+    return G
 
 # then create the graph! how to visualize graphs?????
+
+social_network = generate_graph_from_parameters(8, 8, 5, 0.2, 0.5, 0.8)
+for node in list(social_network.nodes(data = True)):
+    print(node)
+for edge in list(social_network.edges):
+    print(edge)
+
+# source venv/bin/activate
---
+++ simulate.py
@@ -0,0 +1,70 @@
+# pick 2 or 4 starting nodes
+# set a confidence threshold, c
+# for these starting nodes: if they are an English (Chinese) speaker, then the language of their piece of content will be in English (Chinese). If they are bilingual, then randomize it (but have it more likely be in their mother tongue, maybe?).
+# for each English node, they will share the content with the nodes they are connected to provided that:
+# a) political ideology is within c
+# b) speak English
+# for each native Chinese node, they will share the content provided:
+# a) political ideology is within c
+# b) if the sharing node is not bilingual, then they will only pick their Chinese speaking neighbors. If the sharing node is bilingual, then:
+# if the message is in Chinese, they will share with all their Chinese speaking neighbors. For their English speaking neighbors, they will share it with all their English speaking neighbors with probability "Translate and Share" for that node.
+# if the message is in English, they will share with all their English speaking neighbors. For their Chinese speaking neighbors, they will share it with all their Chinese speaking neighbors with probability "Translate and Share" for that node!
+
+import networkx as nx
+import random as rand
+import generate_graph
+
+# first create the social network and initialize parameters
+social_network = generate_graph.generate_graph_from_parameters(8, 8, 5, 0.5, 0.5, 0.5)
+social_network_nodes = list(social_network) # just nodes, no node attributes!
+social_network_edges = list(social_network.edges())
+
+num_starters = 2 # number of nodes that will start with a message
+c = 0.29 # confidence bound
+d = 0.5 # for bilingual nodes, this is the probability that the message they start off will be in Chinese
+message_zero_ideology = 0.13
+message_one_ideology = 0.5
+message_two_ideology = 0.58
+message_three_ideology = 0.9
+
+message_ideologies = [message_zero_ideology, message_one_ideology, message_two_ideology, message_three_ideology]
+
+# then pick the starting nodes and fill in the 'messages' dictionary: key = which message it is [0, 1, 2, or 3], values = {degree of starting node, ideology of content, language of starting node, bilingual or not}
+starters = rand.sample(social_network_nodes, num_starters)
+messages = {}
+for (node, i) in (starters, len(starters)): # node cycles through the list "starters", while i cycles through 0, 1, 2, 3 or 0, 1
+    message_attrs = {}
+
+    # get the degree of the starting node
+    message_attrs["Starting Node Degree"] = social_network.degree[node]
+
+    # get the bilingual status of the starting node
+    message_attrs["Starting Node Bilingual Status"] = social_network.nodes[node]["Node Attributes"]["Bilingual Status"]
+
+    # ideology of starting nodes
+    message_attrs["Message Ideology"] = message_ideologies[i]
+    
+    # get the language of the node: if English, then message is in English; if Chinese and not bilingual, then message is in Chinese; if Chinese and bilingual, then flip the weighted coin
+    if social_network.nodes[node]["Node Attributes"]["Mother Tongue"] == "English":
+        message_attrs["Message Language"] = "English"
+    elif social_network.nodes[node]["Node Attributes"]["Mother Tongue"] == "Chinese" and social_network.nodes[node]["Node Attributes"]["Bilingual Status"] == "Not Bilingual":
+        message_attrs["Message Language"] = "Chinese"
+    elif social_network.nodes[node]["Node Attributes"]["Mother Tongue"] == "Chinese" and social_network.nodes[node]["Node Attributes"]["BIlingual Status"] == "Bilingual":
+        num = rand.uniform(a = 0.0, b = 1.0)
+        if num <= d:
+            message_attrs["Message Language"] = "Chinese"
+        else:
+            message_attrs["Message Language"] = "English"
+
+    messages[i] = message_attrs
+
+# replace the ideology of each starting node with the ideology of the message
+    
+# now run the simulation: create a new graph for each message and compute network statistics at each step
+
+
+#for node in starters:
+    # get the node's language status
+    #if social_network.nodes[node]["Node Attributes"]["Mother Tongue"] == "English":
+        
+
